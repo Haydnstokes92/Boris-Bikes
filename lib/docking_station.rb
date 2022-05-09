e593c9bdee8206533af3aed644cc7051f341fa46
Haydn Stokes <haydnstokes92@gmail.com>
2022-05-09 16:37:07 +0100

    Working back through borris bikes adjusting code and adding an attribute reader for the bike method so it can be used by any instances of this class

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,23 +1,16 @@
 require_relative 'bike'
 
-class DockingStation
-  def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
-  end
-end
 
 class DockingStation
+  attr_reader :bike 
+
+  def release_bike
+    fail 'No bikes available' 
+    Bike.new
+  end
 
   def dock(bike)
     @bike = bike
   end
 end
 
-class DockingStation
-  attr_reader :bike
-end
-
-def release_bike
-  @bike
-end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,6 @@
 require 'docking_station'
 
 describe DockingStation do
-
   describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
@@ -9,19 +8,16 @@ describe DockingStation do
   end
 end
 
-require 'docking_station'
 
 describe DockingStation do
-  describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
-    end
+  it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
 
-require 'docking_station'
 
 describe DockingStation do
   it 'returns docked bikes' do
