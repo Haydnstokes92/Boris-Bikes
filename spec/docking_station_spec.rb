8a3e3a0f2cd781082a7437dc774d22187f09e53b
Haydn Stokes <haydnstokes92@gmail.com>
2022-05-10 08:36:37 +0100

    added a test to ensure no bikes are released unless there is a bike available - will raise and error if fails

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,19 @@ require 'docking_station'
 
 #Unit test for release_bike
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
-  
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+    end
   end
+  
+  # it 'releases working bikes' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
 end
 
 #Unit test to dock bike
@@ -34,5 +41,14 @@ describe DockingStation do
     subject.dock(bike)
     #we need to return the bike we just docked
     expect(subject.bike).to eq bike
+  end
+end
+
+describe DockingStation do
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike .to raise_error 'No bikes avilable' }
+    end
   end
 end
